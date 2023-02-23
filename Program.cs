60bd1eab6692f9b683ef106c0f025e8749ace8c3
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2023-02-23 12:08:11 +0100

    skapar en array av länder

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@
             Land sverige = new Land()
             {
                 namn = "Sverige",
-                styrestyp = "monarki",
+                styrestyp = "Monarki",
                 huvudstad = "Stockholm",
                 invånartal = 10512820
             };
@@ -36,9 +36,41 @@
                 invånartal = 33600
             };
 
-            sverige.Print();
-            tyskland.Print();
-            sanmarino.Print();
+            Land[] länder = new Land[7] 
+            { sverige, tyskland, sanmarino,
+                new Land()
+                {
+                    namn = "Danmark",
+                    styrestyp = "Monarki",
+                    huvudstad = "Köpenhamn",
+                    invånartal = 5928364
+                },
+                new Land()
+                {
+                    namn = "Italien",
+                    styrestyp = "Republik",
+                    huvudstad = "Rom",
+                    invånartal = 58853482
+                },
+                new Land()
+                {
+                    namn = "Tjekien",
+                    styrestyp = "Republik",
+                    huvudstad = "San Marino",
+                    invånartal = 10551219
+                },
+                new Land()
+                {
+                    namn = "Rumänien",
+                    styrestyp = "Republik",
+                    huvudstad = "Bukarest",
+                    invånartal = 19760314
+                }
+            };
+            foreach (Land land in länder)
+            {
+                land.Print();
+            }
         }
     }
 }
